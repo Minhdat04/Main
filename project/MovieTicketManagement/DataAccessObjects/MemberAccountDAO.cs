09d3c0f337ea8304a27a179aef9c53b020f2e606
vhhexcalib <vhhexcalib@gmail.com>
2024-03-19 23:08:32 +0700

    commit 2.0

--- project/MovieTicketManagement/DataAccessObjects/MemberAccountDAO.cs
+++ project/MovieTicketManagement/DataAccessObjects/MemberAccountDAO.cs
@@ -17,6 +17,8 @@ namespace DataAccessObjects
             }
         }
 
+
+
         public static int GetAgeById(int memberId)
         {
             using (MovieTicketManagementContext context = new MovieTicketManagementContext())
@@ -30,6 +32,34 @@ namespace DataAccessObjects
                     age = currentDate.Year - member.Birthday.Year;
                 }
                 return age;
+            }
+        }
+        public static MemberAccount GetMemberByID(int memberID)
+        {
+            using (MovieTicketManagementContext context = new MovieTicketManagementContext())
+            {
+                return context.MemberAccounts.FirstOrDefault(c => c.MemAccId == memberID);
+            }
+        }
+        public static void CreateMember(MemberAccount member)
+        {
+            try
+            {
+                MemberAccount mem1 = GetMemberByID(member.MemAccId);
+                if (mem1 == null)
+                {
+                    using var context = new MovieTicketManagementContext();
+                    context.MemberAccounts.Add(member);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("The member is already exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
         }
     }
